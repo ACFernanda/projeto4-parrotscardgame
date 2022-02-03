67d8eac82610f48738d8e9cfd0acb0ea573050e3
acfernanda <acfernanda98@gmail.com>
2022-02-03 13:24:25 -0300

    jogo insere o numero de cartas viradas para baixo (todas iguais)

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ function questionNumberOfCards() {
 
 questionNumberOfCards();
 
+function dealCards() {
+  const boardGame = document.querySelector("main");
+  for (let i = 0; i < numberOfCards; i++) {
+    boardGame.innerHTML =
+      boardGame.innerHTML +
+      `
+    <div class="card" data-identifier="card" onclick="rotateCard(this)">
+      <div class="shown-face" data-identifier="back-face">
+        <img src="imagens/front.png" alt="parrot-logo" />
+      </div>
+      <div class="hidden-face hide" data-identifier="front-face">
+        <img src="imagens/bobrossparrot.gif" alt="bobrossparrot" />
+      </div>
+    </div>`;
+  }
+}
+
+dealCards();
+
 function rotateCard(cardClass) {
   let shownFace = document.querySelector(".shown-face");
   shownFace.classList.toggle("hide");
